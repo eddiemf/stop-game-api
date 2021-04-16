20f2e941ac0d3fd8b51d1ea3c2c813f58d791d29
Mauricio Dziedzinski <d.mauriciofarias@gmail.com>
2021-04-17 00:47:58 +0200

    refactor: leave game session use case and tests

--- src/application/use-cases/leave-game-session/LeaveGameSession.test.ts
+++ src/application/use-cases/leave-game-session/LeaveGameSession.test.ts
@@ -1,4 +1,5 @@
-import { genericErrors, inputErrors, INPUT_ERROR, VALIDATION_ERROR } from '../../constants';
+import { genericErrors } from '../../constants';
+import { mockWithInputError, mockWithInternalError } from '../../utils';
 import { fakeGameSession } from '../../__mocks__/entities/GameSession.mock';
 import { gameSessionRepositoryMock } from '../../__mocks__/repositories/GameSession.mock';
 import { buildLeaveGameSession } from './LeaveGameSession';
@@ -6,7 +7,7 @@ import { buildLeaveGameSession } from './LeaveGameSession';
 describe('LeaveGameSession', () => {
   const dependencies = {
     gameSessionRepository: gameSessionRepositoryMock,
-    makeGameSession: jest.fn(),
+    findGameSession: jest.fn(),
   };
   const leaveGameSession = buildLeaveGameSession(dependencies);
 
@@ -14,7 +15,7 @@ describe('LeaveGameSession', () => {
     jest.clearAllMocks();
     dependencies.gameSessionRepository.findByHash.mockResolvedValue('mocked game session');
     dependencies.gameSessionRepository.save.mockResolvedValue(undefined);
-    dependencies.makeGameSession.mockReturnValue(fakeGameSession);
+    dependencies.findGameSession.mockReturnValue(fakeGameSession);
     fakeGameSession.getHash.mockReturnValue('mocked hash');
     fakeGameSession.getName.mockReturnValue('mocked name');
     fakeGameSession.getPlayers.mockReturnValue('mocked players');
@@ -22,37 +23,22 @@ describe('LeaveGameSession', () => {
     fakeGameSession.removePlayer.mockReturnValue(undefined);
   });
 
-  it('searches for a game session with the given hash in the game session repository', async () => {
+  it('finds the game session with the given hash', async () => {
     await leaveGameSession({ hash: 'some hash', playerId: 'id' });
-    expect(dependencies.gameSessionRepository.findByHash).toBeCalledWith('some hash');
+    expect(dependencies.findGameSession).toBeCalledWith({ hash: 'some hash' });
   });
 
-  it('throws an internal error if game session repository throws while searching', async () => {
-    dependencies.gameSessionRepository.findByHash.mockRejectedValue('some error');
+  it('throws the caught error if finding game session throws an internal error', async () => {
+    const internalError = mockWithInternalError(dependencies.findGameSession);
     await expect(leaveGameSession({ hash: 'some hash', playerId: 'id' })).rejects.toEqual(
-      genericErrors.INTERNAL_ERROR
+      internalError
     );
   });
 
-  it('throws an input error if game session was not found in the repository', async () => {
-    dependencies.gameSessionRepository.findByHash.mockResolvedValue(null);
+  it('throws the caught error if finding game session throws an input error', async () => {
+    const inputError = mockWithInputError(dependencies.findGameSession);
     await expect(leaveGameSession({ hash: 'some hash', playerId: 'id' })).rejects.toEqual(
-      inputErrors.GAME_SESSION_NOT_FOUND
-    );
-  });
-
-  it('creates a game session entity with the data found in the game session repository', async () => {
-    await leaveGameSession({ hash: 'some hash', playerId: 'id' });
-    expect(dependencies.makeGameSession).toBeCalledWith('mocked game session');
-  });
-
-  it('throws an internal error if game session entity creation throws a validation error', async () => {
-    const validationError = { type: VALIDATION_ERROR, key: 'some key' };
-    dependencies.makeGameSession.mockImplementationOnce(() => {
-      throw validationError;
-    });
-    await expect(leaveGameSession({ hash: 'some hash', playerId: 'id' })).rejects.toEqual(
-      genericErrors.INTERNAL_ERROR
+      inputError
     );
   });
 
@@ -62,10 +48,7 @@ describe('LeaveGameSession', () => {
   });
 
   it('throws the caught error if removing the player throws an input error', async () => {
-    const inputError = { type: INPUT_ERROR, key: 'some key' };
-    fakeGameSession.removePlayer.mockImplementationOnce(() => {
-      throw inputError;
-    });
+    const inputError = mockWithInputError(fakeGameSession.removePlayer);
     await expect(leaveGameSession({ hash: 'some hash', playerId: 'id' })).rejects.toEqual(
       inputError
     );
--- src/application/use-cases/leave-game-session/LeaveGameSession.ts
+++ src/application/use-cases/leave-game-session/LeaveGameSession.ts
@@ -1,10 +1,11 @@
 import { IGameSessionRepository } from '../../../repositories';
-import { genericErrors, inputErrors, INPUT_ERROR, VALIDATION_ERROR } from '../../constants';
-import { IGameSession, IMakeGameSession } from '../../entities/game-session';
+import { genericErrors, INPUT_ERROR, VALIDATION_ERROR } from '../../constants';
+import { IGameSession } from '../../entities/game-session';
+import { IFindGameSession } from '../find-game-session';
 
 interface IDependencies {
   gameSessionRepository: IGameSessionRepository;
-  makeGameSession: IMakeGameSession;
+  findGameSession: IFindGameSession;
 }
 
 interface IProps {
@@ -18,20 +19,11 @@ export interface ILeaveGameSession {
 
 export const buildLeaveGameSession = ({
   gameSessionRepository,
-  makeGameSession,
+  findGameSession,
 }: IDependencies): ILeaveGameSession => {
   const leaveGameSession: ILeaveGameSession = async ({ hash, playerId }) => {
     try {
-      const gameSessionData = await gameSessionRepository.findByHash(hash);
-      if (!gameSessionData) throw inputErrors.GAME_SESSION_NOT_FOUND;
-
-      let gameSession;
-      try {
-        gameSession = makeGameSession(gameSessionData);
-      } catch (error) {
-        throw genericErrors.INTERNAL_ERROR;
-      }
-
+      const gameSession = await findGameSession({ hash });
       gameSession.removePlayer(playerId);
 
       await gameSessionRepository.save({
